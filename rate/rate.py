0e00e04c9885ece49275a2c87e5ef1aec75b2f5b
Piotr Mitros <piotr@mitros.org>
2015-06-18 13:09:31 -0400

    Basic functionality

--- rate/rate.py
+++ rate/rate.py
@@ -1,4 +1,9 @@
-"""TO-DO: Write a description of what this XBlock is."""
+# coding: utf-8
+"""
+This is an XBlock designed to allow people to provide feedback on our
+course resources.
+"""
+
 
 import pkg_resources
 
@@ -9,62 +14,88 @@ from xblock.fragment import Fragment
 
 class RateXBlock(XBlock):
     """
-    TO-DO: document what your XBlock does.
+    This is an XBlock -- eventually, hopefully an aside -- which
+    allows you to rate content in the course. We've wanted this for a
+    long time, but Dartmouth finally encourage me to start to build
+    this.
     """
 
-    # Fields are defined on the class.  You can access them in your code as
-    # self.<fieldname>.
-
-    levels = List(
-        default=0, scope=Scope.settings,
-        help="Names of ratings",
+    mouseover_levels = List(
+        default=["Excellent", "Good", "Average", "Fair", "Poor"],
+        scope=Scope.settings,
+        help="Names of ratings for Likert-like scale"
     )
 
-    prompt = String(
-        default=0, scope=Scope.settings,
-        help="User prompt",
+    mouseover_icons = List(
+        default=[u"😁",u"☺",u"😐",u"☹",u"😟"],
+        scope=Scope.settings,
+        help="Names of ratings for Likert-like scale"
     )
 
-    vote = Integer(
+    string_prompt = String(
+        default="Please provide us feedback on this section.", 
+        scope=Scope.settings,
+        help="Freeform user prompt"
+    )
+
+    likert_prompt = String(
+        default="Please rate your overall experience with this section.", 
+        scope=Scope.settings,
+        help="Likert-like scale user prompt"
+    )
+
+    user_vote = Integer(
         default=-1, scope=Scope.user_state,
-        help="How user voted. -1 if didn't vote",
+        help="How user voted. -1 if didn't vote"
     )
 
     vote_aggregate = List(
-        default=0, scope=Scope.user_state_summary,
-        help="A simple counter, to show something happening",
+        default=None, scope=Scope.user_state_summary,
+        help="A list of user votes"
     )
+
+    user_feedback = String(default = "", scope=Scope.user_state,
+                        help = "Feedback")
 
     def resource_string(self, path):
         """Handy helper for getting resources from our kit."""
         data = pkg_resources.resource_string(__name__, path)
         return data.decode("utf8")
 
-    # TO-DO: change this view to display your data your own way.
     def student_view(self, context=None):
         """
         The primary view of the RateXBlock, shown to students
         when viewing courses.
         """
         html = self.resource_string("static/html/rate.html")
-        frag = Fragment(html.format(self=self))
+        scale_item = u'<span class="rate_likert_rating rate_rating_{i}" title="{level}">{icon}</span>'
+        scale = u"".join(scale_item.format(level=level, icon=icon, i=i) for (level,icon,i) in zip(self.mouseover_levels, self.mouseover_icons, range(len(self.mouseover_icons))))
+        frag = Fragment(html.format(self=self, scale=scale))
         frag.add_css(self.resource_string("static/css/rate.css"))
         frag.add_javascript(self.resource_string("static/js/src/rate.js"))
         frag.initialize_js('RateXBlock')
         return frag
 
-    # TO-DO: change this handler to perform your own actions.  You may need more
-    # than one handler, or you may not need any handlers at all.
     @XBlock.json_handler
-    def increment_count(self, data, suffix=''):
+    def vote(self, data, suffix=''):
         """
-        An example handler, which increments the data.
+        Handle voting
         """
-        # Just to show data coming in...
-        assert data['hello'] == 'world'
+        # Make sure we're initialized
+        if not self.vote_aggregate:
+            self.vote_aggregate = [0]*len(self.mouseover_levels)
 
-        self.count += 1
-        return {"count": self.count}
+        # Remove old vote if we voted before
+        if self.user_vote != -1:
+            self.vote_aggregate[self.vote] -= 1
+
+        self.user_vote = data['vote']
+        self.vote_aggregate[self.user_vote] += 1
+        return {"success": True}
+
+    @XBlock.json_handler
+    def feedback(self, data, suffix=''):
+        self.user_feedback = data['feedback']
 
     # TO-DO: change this to create the scenarios you'd like to see in the
     # workbench while developing your XBlock.
--- rate/static/js/src/rate.js
+++ rate/static/js/src/rate.js
@@ -1,22 +1,39 @@
 /* Javascript for RateXBlock. */
-function RateXBlock(runtime, element) {
+var x;
 
+function RateXBlock(runtime, element) {
+    var vote_handler = runtime.handlerUrl(element, 'vote');
+    var feedback_handler = runtime.handlerUrl(element, 'feedback');
+
+    $('.rate_likert_rating', element).click(function(eventObject) {
+	vote_class = eventObject.currentTarget.className.split(' ').find(function f(x) { return x.startsWith("rate_rating_") } )
+	vote = parseInt(vote_class.split('_')[2]);
+	alert(vote);
+        $.ajax({
+            type: "POST",
+            url: vote_handler,
+            data: JSON.stringify({"vote": vote_class}),
+        });
+    });
+
+    $('.rate_string_area', element).change(function(eventObject) {
+	x = eventObject
+	alert(eventObject.currentTarget.value);
+        $.ajax({
+            type: "POST",
+            url: feedback_handler,
+            data: JSON.stringify({"feedback": eventObject.currentTarget.value}),
+        });
+    });
+
+}
+
+
+/*
     function updateCount(result) {
         $('.count', element).text(result.count);
     }
 
-    var handlerUrl = runtime.handlerUrl(element, 'increment_count');
+    
 
-    $('p', element).click(function(eventObject) {
-        $.ajax({
-            type: "POST",
-            url: handlerUrl,
-            data: JSON.stringify({"hello": "world"}),
-            success: updateCount
-        });
-    });
-
-    $(function ($) {
-        /* Here's where you'd do things on page load. */
-    });
-}
+*/
