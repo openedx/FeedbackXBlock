7edb040e0f5d3c81638a868de6fa3807a168d536
Piotr Mitros <piotr@mitros.org>
2015-06-19 16:22:48 -0400

    Javascript logging

--- rate/static/js/src/rate.js
+++ rate/static/js/src/rate.js
@@ -1,16 +1,29 @@
 /* Javascript for RateXBlock. */
-var x;
+// Work-around so we can log in edx-platform, but not fail in Workbench
+if (typeof Logger === 'undefined') {
+    var Logger = {
+        log: function(a, b) { 
+	    console.log("<<Log>>"); 
+	    console.log(a);
+	    console.log(b);
+	    console.log("<</Log>>"); 
+	}
+    }
+}
+
 
 function RateXBlock(runtime, element) {
     var vote_handler = runtime.handlerUrl(element, 'vote');
     var feedback_handler = runtime.handlerUrl(element, 'feedback');
 
     $('.rate_likert_rating', element).click(function(eventObject) {
-	vote_class = eventObject.currentTarget.className.split(' ').find(function f(x) { return x.startsWith("rate_rating_") } )
-	vote = parseInt(vote_class.split('_')[2]);
+	var vote_class = eventObject.currentTarget.className.split(' ').find(function f(x) { return x.startsWith("rate_rating_") } )
+	var vote = parseInt(vote_class.split('_')[2]);
 	$('.rate_rating_active', element).removeClass("rate_rating_active");
 	$('.'+vote_class, element).addClass("rate_rating_active");
 	
+	Logger.log("edx.recommender.likert_rate", {"vote":vote})
+
         $.ajax({
             type: "POST",
             url: vote_handler,
@@ -20,10 +33,12 @@ function RateXBlock(runtime, element) {
 
     $('.rate_string_area', element).change(function(eventObject) {
 	$('.rate_thank_you', element).css('visibility','hidden');
+	var feedback_string = eventObject.currentTarget.value;
+	Logger.log("edx.recommender.string_feedback", {"feedback":feedback_string})
         $.ajax({
             type: "POST",
             url: feedback_handler,
-            data: JSON.stringify({"feedback": eventObject.currentTarget.value}),
+            data: JSON.stringify({"feedback": feedback_string}),
 	    success: function() {$('.rate_thank_you', element).css('visibility','visible')},
         });
     });
