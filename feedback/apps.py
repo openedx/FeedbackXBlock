fa55752827872b35c2120711f41f6ef8bacd19ef
Cristhian Garcia <crisgarta8@gmail.com>
2023-11-01 10:32:47 -0500

    feat: turn the xblock into an app
    chore: upgrade dependencies

---
+++ feedback/apps.py
@@ -0,0 +1,33 @@
+"""
+forum_email_notifier Django application initialization.
+"""
+
+from django.apps import AppConfig
+
+class FeedbackConfig(AppConfig):
+    """
+    Configuration for the feedback Django application.
+    """
+
+    name = "feedback"
+
+    plugin_app = {
+        "settings_config": {
+            "lms.djangoapp": {
+                "common": {"relative_path": "settings.common"},
+                "test": {"relative_path": "settings.test"},
+                "production": {"relative_path": "settings.production"},
+            },
+            "cms.djangoapp": {
+                "common": {"relative_path": "settings.common"},
+                "test": {"relative_path": "settings.test"},
+                "production": {"relative_path": "settings.production"},
+            },
+        },
+    }
+
+    def ready(self):
+        """
+        Perform initialization tasks required for the forum_email_notifier Django application.
+        """
+        super().ready()
---
+++ feedback/settings/common.py
@@ -0,0 +1,19 @@
+"""
+Common Django settings for eox_hooks project.
+For more information on this file, see
+https://docs.djangoproject.com/en/2.22/topics/settings/
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/2.22/ref/settings/
+"""
+from feedback import ROOT_DIRECTORY
+
+INSTALLED_APPS = [
+    "feedback",
+]
+
+def plugin_settings(settings):
+    """
+    Set of plugin settings used by the Open Edx platform.
+    More info: https://github.com/edx/edx-platform/blob/master/openedx/core/djangoapps/plugins/README.rst
+    """
+    settings.MAKO_TEMPLATE_DIRS_BASE.append(ROOT_DIRECTORY / "templates")
