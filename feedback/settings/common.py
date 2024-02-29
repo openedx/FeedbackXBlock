50fad54e35c30dbe460ff7e7438ec7f5ab74e64d
Tim McCormack <tmccormack@edx.org>
2024-02-29 15:10:27 +0000

    docs: Fix link now that edx-platform redirect is broken
    See https://discuss.openedx.org/t/please-update-your-git-urls-for-edx-platform-and-several-other-repos/12387

--- feedback/settings/common.py
+++ feedback/settings/common.py
@@ -15,6 +15,6 @@ INSTALLED_APPS = [
 def plugin_settings(settings):
     """
     Set of plugin settings used by the Open Edx platform.
-    More info: https://github.com/edx/edx-platform/blob/master/openedx/core/djangoapps/plugins/README.rst
+    More info: https://github.com/openedx/edx-platform/blob/master/openedx/core/djangoapps/plugins/README.rst
     """
     settings.MAKO_TEMPLATE_DIRS_BASE.append(ROOT_DIRECTORY / "templates")
--- feedback/settings/production.py
+++ feedback/settings/production.py
@@ -10,5 +10,5 @@ https://docs.djangoproject.com/en/2.22/ref/settings/
 def plugin_settings(settings):  # pylint: disable=unused-argument
     """
     Set of plugin settings used by the Open Edx platform.
-    More info: https://github.com/edx/edx-platform/blob/master/openedx/core/djangoapps/plugins/README.rst
+    More info: https://github.com/openedx/edx-platform/blob/master/openedx/core/djangoapps/plugins/README.rst
     """
